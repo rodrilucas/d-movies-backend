63e0a7a386781bb810f80e11d867fbefd040fe9c
rodrilucas <lucas.celery@gmail.com>
2025-08-03 17:10:32 -0300

    feat: handle the sort parameter

---
+++ src/utils/parse-sort-param.ts
@@ -0,0 +1,21 @@
+import type { SortBy, SortOrder } from '@/services/movies-service'
+
+export type SortOptions = {
+  sortBy?: SortBy
+  sortOrder?: SortOrder
+}
+export function parseSortParam(sort?: string): SortOptions {
+  if (!sort) return {}
+  const [maybeField, maybeDir] = sort.split('.') as [SortBy, SortOrder]
+  const validBy = new Set<SortBy>([
+    'popularity',
+    'release_date',
+    'vote_average',
+    'vote_count',
+  ])
+  const validOrder = new Set<SortOrder>(['asc', 'desc'])
+  return {
+    sortBy: validBy.has(maybeField) ? maybeField : undefined,
+    sortOrder: validOrder.has(maybeDir) ? maybeDir : undefined,
+  }
+}
