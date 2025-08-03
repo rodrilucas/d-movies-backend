cfb445f1225b3905f7f653ad319b1b97cd62e2ef
rodrilucas <lucas.celery@gmail.com>
2025-08-03 17:12:48 -0300

    feat: create the movie route

---
+++ src/web/routes/movies-routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+import { makeMoviesController } from '../factories/make-movies-controller'
+
+export function moviesRoutes(app: FastifyInstance) {
+  const moviesControler = makeMoviesController()
+  app.get('/', moviesControler.getAll)
+}
