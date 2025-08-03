2ba235cd296d863f487b2acc5193e4bb318ab248
rodrilucas <lucas.celery@gmail.com>
2025-08-03 17:13:55 -0300

    feat: register route and handle application error

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
 import fastify from 'fastify'
+import { moviesRoutes } from './web/routes/movies-routes'
+import { errorHandler } from './errors/error-handler'
 
 export const app = fastify()
+
+app.register(moviesRoutes, { prefix: '/movies' })
+
+app.setErrorHandler(errorHandler)
