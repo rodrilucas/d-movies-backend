d7932b718864d87ed4d8882d06e77146de31fe60
rodrilucas <lucas.celery@gmail.com>
2025-08-04 16:57:03 -0300

    feat: throw instance errors in movies controller

--- src/web/controllers/movies-controller.ts
+++ src/web/controllers/movies-controller.ts
@@ -8,6 +8,7 @@ import { MoviesPageService } from '@/services/movies-page-service'
 import { makeTmdbService } from '@/services/factories/make-tmdb-service'
 import { getByFiltersDto } from '../dto/get-by-filters-dto'
 import { getByKeywordDto } from '../dto/get-by-keyword.dto'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
 
 export class MoviesController {
   constructor(
@@ -19,85 +20,112 @@ export class MoviesController {
     const { page, limit, sort } = getAllDto.parse(req.query)
     const { sortBy, sortOrder } = parseSortParam(sort)
 
-    const movies = this.moviesService.getAll({
+    const movies = await this.moviesService.getAll({
       page,
       limit,
       sortBy,
       sortOrder,
     })
 
-    return rep.status(200).send({ movies })
+    const [{ count }] = await this.moviesService.countAll()
+
+    return rep.status(200).send({
+      currentPage: page,
+      totalPages: Math.ceil(count / limit),
+      limit,
+      totalMovies: Number(count),
+      movies,
+    })
   }
 
   getById = async (req: FastifyRequest, rep: FastifyReply) => {
     const { id } = getByIdDto.parse(req.params)
-    const movie = await this.moviesService.getById({ id })
-    rep.status(200).send({ movie })
+    try {
+      const movie = await this.moviesService.getById({ id })
+      rep.status(200).send({ movie })
+    } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        rep.status(404).send({ error: error.message })
+      }
+      throw error
+    }
   }
 
   search = async (req: FastifyRequest, rep: FastifyReply) => {
     const { query, page, limit, sort } = searchDto.parse(req.query)
     const { sortBy, sortOrder } = parseSortParam(sort)
 
+    const tmdbService = makeTmdbService()
+
     const pageData = await this.moviesPageService.getPage({
       page,
       query,
     })
 
-    if (pageData) {
-      const movies = await this.moviesService.getByIds({
+    try {
+      if (pageData) {
+        const movies = await this.moviesService.getByIds({
+          page,
+          limit,
+          movieIds: pageData.movie_ids,
+          sortBy,
+          sortOrder,
+        })
+        return rep.status(200).send({
+          page,
+          limit,
+          totalPages: pageData?.total_pages,
+          totalResults: pageData?.total_results,
+          movies,
+        })
+      }
+
+      const moviesFromAPI = await tmdbService.getPage({
+        query,
         page,
         limit,
-        movieIds: pageData.movie_ids,
         sortBy,
         sortOrder,
       })
 
       return rep.status(200).send({
         page,
-        totalPages: pageData.total_pages,
-        totalResults: pageData.total_results,
         limit,
-        movies,
+        totalPages: moviesFromAPI.total_pages,
+        totalResults: moviesFromAPI.total_results,
+        movies: moviesFromAPI.results,
       })
+    } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        rep.status(404).send({ error: error.message })
+      }
+      throw error
     }
-
-    const tmdbService = makeTmdbService()
-    const resultsFromApi = await tmdbService.getPage({
-      query,
-      page,
-      limit,
-      sortBy,
-      sortOrder,
-    })
-
-    return rep.status(200).send({
-      page,
-      ...resultsFromApi,
-    })
   }
 
-  getByFilters = async (req: FastifyRequest, reply: FastifyReply) => {
+  getByFilters = async (req: FastifyRequest, rep: FastifyReply) => {
     const filters = getByFiltersDto.parse(req.body)
     const { sortBy, sortOrder } = parseSortParam(filters.sort)
 
-    const { movies, page, totalMovies, totalPages } =
-      await this.moviesService.getByFilters({
-        filters: {
-          ...filters,
-          sort: { by: sortBy, order: sortOrder },
-        },
-      })
+    const [{ count }] = await this.moviesService.countAll()
 
-    reply.status(200).send({
-      page,
-      totalMovies,
-      totalPages,
+    const movies = await this.moviesService.getByFilters({
+      filters: {
+        ...filters,
+        sort: { by: sortBy, order: sortOrder },
+      },
+    })
+
+    return rep.status(200).send({
+      page: filters.page,
+      limit: filters.limit,
+      totalPages: Math.ceil(count / filters.limit),
+      totalMovies: count,
       movies,
     })
   }
 
-  getByKeyword = async (req: FastifyRequest, reply: FastifyReply) => {
+  getByKeyword = async (req: FastifyRequest, rep: FastifyReply) => {
     const { keyword, limit } = getByKeywordDto.parse(req.query)
 
     const movies = await this.moviesService.getByKeyword({
@@ -105,7 +133,8 @@ export class MoviesController {
       limit,
     })
 
-    reply.status(200).send({
+    rep.status(200).send({
+      limit,
       movies,
     })
   }
