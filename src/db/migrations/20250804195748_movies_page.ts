3a40235c109a8e4d769ab0a461a358eeab40e32e
rodrilucas <lucas.celery@gmail.com>
2025-08-04 17:12:05 -0300

    chore: movies page and users migrations

---
+++ src/db/migrations/20250804195748_movies_page.ts
@@ -0,0 +1,18 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTableIfNotExists('movies_page', (table) => {
+    table.increments('id').primary()
+    table.string('query')
+    table.string('normalized_query')
+    table.integer('page_number')
+    table.specificType('movie_ids', 'integer[]')
+    table.integer('total_pages')
+    table.integer('total_results')
+    table.unique(['normalized_query', 'page_number'])
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.schema.dropTableIfExists('movies_page')
+}
---
+++ src/db/migrations/20250804195923_users.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTableIfNotExists('users', (table) => {
+    table.increments('id').primary()
+    table.string('first_name').notNullable()
+    table.string('last_name').nullable()
+    table.string('email').notNullable().unique()
+    table.string('password_hash').notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.schema.dropTableIfExists('users')
+}
