7da7fcce89338340af0d721339813abc689ec1ae
rodrilucas <lucas.celery@gmail.com>
2025-08-05 10:17:44 -0300

    feat: register fastify jwt

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import fastify from 'fastify'
 import { moviesRoutes } from './web/routes/movies/movies-routes'
 import { errorHandler } from './errors/error-handler'
 import { usersRoutes } from './web/routes/users/users-routes'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env'
 
 export const app = fastify()
 
+app.register(fastifyJwt, { secret: env.JWT_SECRET })
 app.register(moviesRoutes, { prefix: '/movies' })
 app.register(usersRoutes, { prefix: '/users' })
 
