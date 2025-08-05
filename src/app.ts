89d7705589ef85229587715b227fde688a1d7579
rodrilucas <lucas.celery@gmail.com>
2025-08-05 12:04:42 -0300

    feat: register the auth route and add token expiration time

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import { errorHandler } from './errors/error-handler'
 import { usersRoutes } from './web/routes/users/users-routes'
 import fastifyJwt from '@fastify/jwt'
 import { env } from './env'
+import { authRoutes } from './web/routes/auth/auth-routes'
 
 export const app = fastify()
 
-app.register(fastifyJwt, { secret: env.JWT_SECRET })
+app.register(fastifyJwt, { secret: env.JWT_SECRET, sign: { expiresIn: '1d' } })
+app.register(authRoutes, { prefix: '/auth' })
 app.register(moviesRoutes, { prefix: '/movies' })
 app.register(usersRoutes, { prefix: '/users' })
 
