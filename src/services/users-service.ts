8e8c2e6ad9d01c4da79afa11cc55cda7faee18e0
rodrilucas <lucas.celery@gmail.com>
2025-08-05 10:15:45 -0300

    feat: add authentication method to movies services

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -5,8 +5,14 @@ import {
   UsersRepository,
 } from '@/repositories/contracts/users-repository'
 import { AlreadyExistsError } from '@/web/errors/already-exists-error'
+import { InvalidCredentials } from '@/web/errors/invalid-credentials'
+import { compare } from 'bcryptjs'
 
 type RegisterParam = SaveParam
+type AuthenticateParam = {
+  email: string
+  password: string
+}
 type GetByIdParam = FindByIdParam
 type GetByEmailParam = FindByEmailParam
 
@@ -23,6 +29,22 @@ export class UsersService {
     }
 
     await this.usersRepository.save({ user })
+  }
+
+  authenticate = async ({ email, password }: AuthenticateParam) => {
+    const user = await this.usersRepository.findByEmail({ email })
+
+    if (!user) {
+      throw new InvalidCredentials()
+    }
+
+    const passwordMatches = await compare(password, user.password_hash)
+
+    if (!passwordMatches) {
+      throw new InvalidCredentials()
+    }
+
+    return user
   }
 
   getById = async ({ id }: GetByIdParam) => {
