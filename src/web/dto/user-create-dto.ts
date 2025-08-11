d2e95d73d34a0e1756fc86a648399791de548e81
rodrilucas <lucas.celery@gmail.com>
2025-08-11 18:57:38 -0300

    fix: validation dto to create user

--- src/web/dto/user-create-dto.ts
+++ src/web/dto/user-create-dto.ts
@@ -1,11 +1,15 @@
 import z from 'zod'
 
 export const userCreateDto = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
+  firstName: z
+    .string({ error: 'O primeiro nome é obrigatório' })
+    .min(1, 'O primeiro nome não pode estar vazio'),
+  lastName: z
+    .string({ error: 'O sobrenome é obrigatório' })
+    .min(1, 'O sobrenome não pode estar vazio'),
   email: z.email('Email inválido'),
   password: z
-    .string()
+    .string({ error: 'A senha é obrigatória' })
     .min(6, "A senha não pode ser menor que '6' caracteres")
     .max(100, "A senha pode ter no máximo '100' caracteres"),
 })
