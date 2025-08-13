84865210d2da33bd51d975359f97f50797161ad5
rodrilucas <lucas.celery@gmail.com>
2025-08-13 10:45:46 -0300

    server port

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
   TMDB_URL: z.string(),
   TMDB_TOKEN: z.string(),
   JWT_SECRET: z.string(),
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { env } from './env'
 import { app } from './app'
+import { env } from './env'
 
 app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'))
